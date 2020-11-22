309621804c2bff0698b77abf7b2cc8f59e566375
bayanalalem <bayanalalem0@gmail.com>
2020-11-22 19:28:58 +0200

    done

---
+++ src/form.js
@@ -0,0 +1,64 @@
+import { useState } from 'react';
+import { Button,Form } from 'react-bootstrap';
+
+function TodoForm(props) {
+  const [item, setItem] = useState({});
+ const handleInputChange = e => {
+     console.log("vv",e.target)
+    setItem({...item, [e.target.name]: e.target.value } )
+  };
+
+const  handleSubmit = (e) => {
+    e.preventDefault();
+    e.target.reset();
+    console.log("itemform",item)
+   props.handleSubmit(item);
+    const item1 = {};
+    setItem({item:item1})
+  };
+  return (
+    <>
+    <form onSubmit={handleSubmit}>
+    <Form.Group controlId="formBasicEmail" fluid="md">
+    <Form.Label><span>To Do Item</span>
+    <Form.Control name="text"
+              placeholder="Add To Do List Item"
+              onChange={handleInputChange} />
+    </Form.Label>
+    <Form.Label><span>Difficulty Rating</span>
+    <Form.Control defaultValue="1" type="range" min="1" max="5" name="difficulty" onChange={handleInputChange}  />
+    </Form.Label>
+    <Form.Label><span>Assigned To</span>
+    <Form.Control type="text" name="assignee" placeholder="Assigned To" onChange={handleInputChange}  />
+    </Form.Label>
+    <Button variant="primary" type="submit">
+    Add Item
+  </Button>
+  </Form.Group>
+  </form>
+  
+       {/* <h3>Add Item</h3>
+        <form onSubmit={handleSubmit}>
+          <label>
+            <span>To Do Item</span>
+            <input
+              name="text"
+              placeholder="Add To Do List Item"
+              onChange={handleInputChange}
+            />
+          </label>
+          <label>
+            <span>Difficulty Rating</span>
+            <input defaultValue="1" type="range" min="1" max="5" name="difficulty" onChange={handleInputChange} />
+          </label>
+          <label>
+            <span>Assigned To</span>
+            <input type="text" name="assignee" placeholder="Assigned To" onChange={handleInputChange} />
+          </label>
+          <button>Add Item</button>
+        </form> */}
+    </>
+  );
+}
+
+export default TodoForm;
---
+++ src/todo.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import './todo.scss';
+import { Container,Col,Row,Navbar } from 'react-bootstrap';
+import TodoForm from './form.js';
+import TodoList from './list.js';
+import {  useState, useEffect } from 'react';
+
+function ToDo(props) {
+  const [list, setList] = useState([]);
+
+ const  addItem = (item) => {
+     console.log(item)
+    item._id = Math.random();
+    item.complete = false;
+  setList(  [...list, item])
+  };
+ const toggleComplete = id => {
+
+    let item = list.filter(i => i._id === id)[0] || {};
+
+    if (item._id) {
+      item.complete = !item.complete;
+      let updateList = list.map(listItem => listItem._id === item._id ? item : listItem);
+      setList(updateList)
+    }
+
+  }
+  
+useEffect(() => {
+        let list1 = [
+        { _id: 1, complete: false, text: 'Clean the Kitchen', difficulty: 3, assignee: 'Person A'},
+        { _id: 2, complete: false, text: 'Do the Laundry', difficulty: 2, assignee: 'Person A'},
+        { _id: 3, complete: false, text: 'Walk the Dog', difficulty: 4, assignee: 'Person B'},
+        { _id: 4, complete: true, text: 'Do Homework', difficulty: 3, assignee: 'Person C'},
+        { _id: 5, complete: false, text: 'Take a Nap', difficulty: 1, assignee: 'Person B'},
+      ];
+  
+    setList(list1);
+  },[] );
+
+  return (
+    <>
+     
+  
+     <Navbar bg="primary" variant="dark">
+     <Navbar.Brand href="#home">HOME</Navbar.Brand>
+     </Navbar>
+     <Container fluid="md"> 
+     <header>
+        <Row> 
+     < Col className="colfun">There are {list.filter(item => !item.complete).length} Items To Complete</Col>
+    
+     </Row>
+    </header>
+    <Row> 
+     < Col ><Container className="todo">
+
+<div>
+  <TodoForm handleSubmit={addItem} />
+</div>
+
+<div>
+  <TodoList
+    list={list}
+    handleComplete={toggleComplete}
+  />
+</div>
+</Container></Col>
+    
+     </Row>
+        </Container>
+
+    </>
+  );
+}
+
+export default ToDo;
